9a1241b3f640dab21f533d76dafda872ad5740ce
Rafael Martins Pereira Chianca <rafael.chianca@CTNDELLGDHX783.pampa.compasso>
2021-01-15 22:13:49 -0300

    Home page styling

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,29 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { Text } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
+import ArticleList from '../../components/Lists/NewsList';
 import { listTechnologyRequested } from '../../store/actions/news';
-import { Container } from './styles';
+import { Container, Header } from './styles';
 
 export default function Home() {
 
     const dispatch = useDispatch();
-    const tecnology = useSelector(state => state.news.tecnology);
+    const tecnology = useSelector(state => state.news.technology);
     const [techNews, setTechNews] = useState([]);
 
-    // useEffect(() => {
-    //     dispatch(listTechnologyRequested(1, 20));
-    // }, [])
+    useEffect(() => {
+        dispatch(listTechnologyRequested(1, 20));
+    }, [])
 
-    // useEffect(() => {
-    //     setTechNews(tecnology);
-    // }, [tecnology])
+    useEffect(() => {
+        setTechNews(tecnology);
+        console.log('resposta do sagas', tecnology);
+    }, [tecnology])
 
     return (
         <Container>
-            <Text>Hello World</Text>
-            {techNews && techNews.length > 0 &&
-                <Text>{techNews[0]}</Text>
-            }
+            <Header>Technology</Header>
+            <ArticleList articles={techNews} />
         </Container>
     );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,8 +1,15 @@
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
 
 export const Container = styled(SafeAreaView)`
     flex: 1;
-    background-color: white;
+    background-color: red;
     padding: 20px;
+`;
+
+export const Header = styled.Text`
+    font-weight: bold;
+    font-size: 30px;
+    align-self: center;
+    margin-bottom: 20px;
 `;
