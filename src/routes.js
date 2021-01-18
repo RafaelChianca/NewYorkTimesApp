de3cd263bcb5fa44b40d1289ae2fdf6dc44a844d
Rafael Martins Pereira Chianca <rafael.chianca@CTNDELLGDHX783.pampa.compasso>
2021-01-18 11:48:35 -0300

    Creates a tab navigator

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,48 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, TabActions } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as Unicons from '@iconscout/react-native-unicons';
 import Home from './pages/Home';
+import Science from './pages/Science';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function Routes() {
     return (
         <NavigationContainer>
-            <Stack.Navigator
+             <Tab.Navigator
                 initialRouteName="Home"
-                screenOptions={{
-                    headerShown: false,
-                    headerTintColor: '#FFF'
+                tabBarOptions={{
+                    keyboardHidesTabBar: true,
+                    style: {
+                        backgroundColor: 'white',
+                        borderTopColor: 'rgba(255, 255, 255, 0.2)',
+                        borderTopWidth: 1,
+                    },
+                    showLabel: true,
+                    activeTintColor: 'black'
                 }}
             >
-                <Stack.Screen name="Home" component={Home} />
-            </Stack.Navigator>
+                <Tab.Screen 
+                    name="Tech"
+                    component={Home}
+                    options={{
+                        tabBarIcon: ({ focused }) => (
+                            <Unicons.UilLightbulbAlt size={28} color={focused ? 'black' : 'lightgrey'}/>
+                        ),
+                    }}
+                />
+                <Tab.Screen 
+                    name="Science"
+                    component={Science}
+                    options={{
+                        tabBarIcon: ({ focused }) => (
+                            <Unicons.UilMicroscope size={28} color={focused ? 'black' : 'lightgrey'}/>
+                        ),
+                    }}
+                />
+            </Tab.Navigator>
+            
         </NavigationContainer>
     );
 }
