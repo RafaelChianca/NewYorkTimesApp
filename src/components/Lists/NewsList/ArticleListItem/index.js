49e8e24693271178e999167bc686cdc9cf6643ed
Rafael Martins Pereira Chianca <rafael.chianca@CTNDELLGDHX783.pampa.compasso>
2021-01-18 11:07:51 -0300

    Adds image to article preview

--- src/components/Lists/NewsList/ArticleListItem/index.js
+++ src/components/Lists/NewsList/ArticleListItem/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { ArticleContainer, ArticleImage, Title, Headline } from './styles';
+import React, { useState } from 'react';
+import FastImage from 'react-native-fast-image';
+import ArticleModal from '../../../Modals/ArticleModal';
+import { ArticleContainer, ArticleImage, Title, Abstract } from './styles';
 
 export default function ArticleListItem ({ article }) {
 
+    const [modalVisible, setModalVisible] = useState(false);
+
     return (
-        <ArticleContainer>
-            <ArticleImage  />
-            <Title numberOfLines={2}>{article.title}</Title>
-            <Headline numberOfLines={3}>{article.abstract}</Headline>
-        </ArticleContainer>
+        <>
+            {article &&
+                <>
+                    <ArticleContainer onPress={() => {setModalVisible(true)}}>
+                        <Title numberOfLines={2}>{article.title ? article.title : ''}</Title>
+                        <ArticleImage
+                            source={{
+                                uri: article.multimedia && article.multimedia[0]
+                                    ? article.multimedia[0].url
+                                    : '',
+                                headers: { Authorization: 'someAuthToken' },
+                                priority: FastImage.priority.normal,
+                            }}
+                            style={{ resizeMode: 'contain' }}
+                        />
+                        {/* <Title numberOfLines={2}>{article.title ? article.title : ''}</Title> */}
+                        <Abstract>{article.abstract ? article.abstract : ''}</Abstract>
+                    </ArticleContainer>
+                    <ArticleModal article={article} modalVisible={modalVisible} setModalVisible={setModalVisible} />
+                </>
+            }
+        </>
     );
 }
